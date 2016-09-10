7b12bbde56b6bfac3efcddc4a6e7ac15641750fd
giannosantoniou <giannos.antoniou@gmail.com>
2016-09-10 11:26:08 +0000

    types folder

---
+++ DBSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FPP_Library.Types
+{
+    public class DBSettings
+    { 
+        public static string CS = "Server=tcp:azayiagroup-server.database.windows.net,1433;Database=FPP;User ID = gantoniou@azayiagroup-server;Password=azayia!00;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;";
+    }
+}
---
+++ Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FPP_Library.Types
+{
+    public class Position
+    {
+        public double Lat { get; set; } 
+        public double Lng { get; set; }
+    }
+}
