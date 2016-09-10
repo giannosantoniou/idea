4411395f9fcd128d076b17cd96ee0aba09b38e25
giannosantoniou <giannos.antoniou@gmail.com>
2016-09-10 11:27:49 +0000

    password

--- DBSettings.cs
+++ DBSettings.cs
@@ -7,6 +7,6 @@ namespace FPP_Library.Types
 {
     public class DBSettings
     { 
-        public static string CS = "Server=tcp:azayiagroup-server.database.windows.net,1433;Database=FPP;User ID = gantoniou@azayiagroup-server;Password=azayia!00;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;";
+        public static string CS = "Server=tcp:azayiagroup-server.database.windows.net,1433;Database=FPP;User ID = gantoniouerver;Password=fsdfdsf;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;";
     }
 }
